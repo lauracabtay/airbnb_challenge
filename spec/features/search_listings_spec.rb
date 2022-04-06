6d1a80651bda06b88901fa48e166005cbe1447c0
Laura Cabantous <laura.cabantous@gmail.com>
2022-04-06 17:24:30 +0100

    Edited feature test for search

--- spec/features/search_listings_spec.rb
+++ spec/features/search_listings_spec.rb
@@ -1,56 +1,29 @@
 require 'pg'
 
 feature "search listings" do
-  scenario "user can search listings by a location" do
-    connection = PG.connect(dbname: "makersbnb_test")
-    connection.exec("INSERT INTO listing (title, description, location, price_per_night)
-    values (
-            '2 bed  apartment',
-            'Lorem ipsum dolor sit amet',
-            'Hackney',
-            350.00
-        );")
-    connection.exec("insert into listing (title, description, location, price_per_night)
-    values (
-            'Lovely studio in Camden Town',
-            'Lorem ipsum dolor sit amet.',
-            'Camden Town',
-            280.00
-        );")
-    connection.exec("insert into listing (title, description, location, price_per_night)
-    values (
-            '3 bed flat in Camden Town',
-            'Lorem ipsum dolor sit amet.',
-            'Camden Town',
-            900.00
-        );")
+  scenario "user can search listings by location, keyword and max-price" do
 
-    connection.exec("insert into listing (title, description, location, price_per_night)
-    values (
-            'Large 6 bed house',
-            'Lorem ipsum dolor sit amet.',
-            'Harlesden',
-            800.00
-        );")
+    Listing.create(title: '2 bed apartment', description: 'Lorem ipsum dolor sit amet', location:'London', price_per_night: 350.00)
+    Listing.create(title: 'Lovely studio in Camden Town', description: 'Lorem ipsum dolor sit amet', location:'Camden Town', price_per_night: 280.00)
+    Listing.create(title: '3 bed flat in Camden Town', description: 'Lorem ipsum dolor sit amet', location:'Camden Town', price_per_night: 900.00)
+    Listing.create(title: 'Large 6 bed house', description: 'Lorem ipsum dolor sit amet', location:'Harlesden', price_per_night: 800.00)
     
     visit("/listings")
-
     fill_in('location', with: 'Camden Town')
-    fill_in('max_price', with: '900')
+    fill_in('keyword', with: 'studio')
+    fill_in('max_price', with: '600')
     click_button('Search')
   
-
     expect(page).to have_content ('Lovely studio in Camden Town')
-    expect(page).to have_content ('3 bed flat in Camden Town')
+    expect(page).not_to have_content ('3 bed flat in Camden Town')
     expect(page).not_to have_content ('Large 6 bed house')
   end
 
-  scenario 'user can search listing by keyword' do 
+  scenario 'user can search listing by keyword and max price' do 
     Listing.create(title: '2 bed  apartment', description: 'Lorem ipsum dolor sit amet', location:'London', price_per_night: 350.00)
     Listing.create(title: 'Lovely studio in Camden Town', description: 'Lorem ipsum dolor sit amet', location:'London', price_per_night: 280.00)
     
     visit("/listings")
-
     fill_in('location', with: 'London')
     fill_in('keyword', with: 'studio')
     fill_in('max_price', with: '600')
