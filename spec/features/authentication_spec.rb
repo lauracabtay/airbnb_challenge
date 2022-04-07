a0d08b75987f47d360094d37ea3541528858b29f
Laura Cabantous <laura.cabantous@gmail.com>
2022-04-07 16:45:55 +0100

    add sign out functionality

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -36,4 +36,17 @@ feature 'authentication' do
         expect(page).to have_content 'Wrong username or password. Try again or register now.'
         expect(page).not_to have_content 'Welcome laila123!'
     end
+
+    scenario "user can sign out" do
+        User.register(username:"laila123", password: "randompassword")
+        visit '/'
+        click_button('Sign in')
+        fill_in(:username, with: 'laila123')
+        fill_in(:password, with: 'randompassword')
+        click_button('Submit')
+        click_button('Sign out')
+
+        expect(page).to have_content "You have signed out."
+        expect(page).not_to have_content 'Welcome, test@example.com'
+    end
 end
