c6fd1b83e168a2947c61903719025268e6a18eec
Laura Cabantous <laura.cabantous@gmail.com>
2022-04-08 10:48:46 +0100

    Create My Listings space

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,14 +3,6 @@ require 'pg'
 def setup_test_database
   p "Setting up test database..."
     connection = PG.connect(dbname: 'makersbnb_test')
-    # Clear the listing table
-    connection.exec("TRUNCATE listing;")
-    # Clear the listing table
-    connection.exec("TRUNCATE booking;")
-    # Clear the listing table
-    connection.exec("TRUNCATE calendar;")
-    # Clear the listing table
-    connection.exec("TRUNCATE users;")
-    # Clear the listing table
-    connection.exec("TRUNCATE hosts;")
+    # Clear the tables
+    connection.exec("TRUNCATE listing, booking, calendar, users, hosts;")
 end
